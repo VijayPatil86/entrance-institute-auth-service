ea70ef0385f4533c3c1e35c6db6cdf6574d3a2bb
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-06 18:34:51 +0530

    initial commit

---
+++ src/main/java/com/neec/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.neec.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Builder @NoArgsConstructor @AllArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) @Getter
+public class LoginResponseDTO {
+	String jwtToken;
+}
