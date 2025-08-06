95cb9dfadf66df99aee9c97423fa4c2fda32a0c5
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-06 18:29:57 +0530

    added @Schema Swagger tag

--- src/main/java/com/neec/dto/LoginRequestDTO.java
+++ src/main/java/com/neec/dto/LoginRequestDTO.java
@@ -1,5 +1,6 @@
 package com.neec.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,11 +14,13 @@ import lombok.experimental.FieldDefaults;
 
 @Builder @NoArgsConstructor @AllArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) @Getter @Setter
 public class LoginRequestDTO {
+	@Schema(description = "email address to login", example = "john.doe@gmail.com")
 	@NotBlank(message = "Email address cannot be blank.")
 	@Email(message = "Please provide a valid email address.")
 	@Pattern(message = "Please provide a valid email address.", regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
 	private String emailAddress;
 	
+	@Schema(description = "Password to login", example = "P@$$w0rd")
 	@NotBlank(message = "Password cannot be blank.")
 	private String password;
 }
