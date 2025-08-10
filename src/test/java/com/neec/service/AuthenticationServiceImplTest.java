c5cc48d782248c54a48375aad0983733046e2cfc
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-10 15:57:30 +0530

    added UT for publishing message using RabbitMQ

--- src/test/java/com/neec/service/AuthenticationServiceImplTest.java
+++ src/test/java/com/neec/service/AuthenticationServiceImplTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.neec.dto.RegistrationRequestDTO;
 import com.neec.entity.UserLogin;
@@ -31,6 +34,8 @@ public class AuthenticationServiceImplTest {
 	private UserLoginRepository mockUserLoginRepository;
 	@Mock
 	private BCryptPasswordEncoder mockBCryptPasswordEncoder;
+	@Mock
+	private RabbitTemplate mockRabbitTemplate;
 	@InjectMocks
 	private AuthenticationServiceImpl authenticationServiceImpl;
 	
@@ -84,5 +89,42 @@ public class AuthenticationServiceImplTest {
 		assertEquals(EnumUserAccountStatus.PENDING_VERIFICATION, tobeSavedUserLogin.getAccountStatus());
 		assertNotNull(tobeSavedUserLogin.getVerificationToken(), "Expected: verification token must be set");
 		assertNotNull(tobeSavedUserLogin.getVerificationTokenExpiresAt(), "Expected: verification token expiring datetime must be set");
+	}
+
+	@Test
+	void test_registerUser_NewEmailAddress_Save_User_SendEmail() {
+		ReflectionTestUtils.setField(authenticationServiceImpl, "topicExchangeName", "email_exchange");
+		ReflectionTestUtils.setField(authenticationServiceImpl, "routingKey", "routing.key");
+
+		RegistrationRequestDTO dto = RegistrationRequestDTO.builder()
+				.emailAddress("new.email.address@gmail.com")
+				.password("P@$$w0rd")
+				.build();
+		when(mockUserLoginRepository.findByEmailAddress("new.email.address@gmail.com"))
+			.thenReturn(Optional.empty());
+		when(mockBCryptPasswordEncoder.encode(dto.getPassword()))
+			.thenReturn("hashed-password");
+		UserLogin mockSavedUser = UserLogin.builder()
+				.emailAddress("new.email.address@gmail.com")
+				.verificationToken("7775c963-1d25-4f0b-b89d-b1b1d88cc4cb")
+				.build();
+		when(mockUserLoginRepository.save(any(UserLogin.class))).thenReturn(mockSavedUser);
+
+		authenticationServiceImpl.registerUser(dto);
+
+		verify(mockUserLoginRepository).findByEmailAddress("new.email.address@gmail.com");
+		verify(mockUserLoginRepository, times(1)).save(any(UserLogin.class));
+
+		ArgumentCaptor<String> emailExchangeNameCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> routingKeyCaptor = ArgumentCaptor.forClass(String.class);
+		@SuppressWarnings("unchecked")
+		ArgumentCaptor<Map<String, String>> messageCaptor = ArgumentCaptor.forClass(Map.class);
+		verify(mockRabbitTemplate).convertAndSend(emailExchangeNameCaptor.capture(),
+				routingKeyCaptor.capture(), messageCaptor.capture());
+		assertEquals("email_exchange", emailExchangeNameCaptor.getValue(), "Expected: correct Exchange name must be set");
+		assertEquals("routing.key", routingKeyCaptor.getValue(), "Expected: correct message routing key must be set");
+		Map<String, String> messageSent = messageCaptor.getValue();
+		assertEquals("new.email.address@gmail.com", messageSent.get("email"), "Expected: correct email must be set");
+		assertEquals("7775c963-1d25-4f0b-b89d-b1b1d88cc4cb", messageSent.get("token"), "Expected: correct token be set");
 	}
 }
