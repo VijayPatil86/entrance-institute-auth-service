7bdde6abd17011aabc7c0948c7a79e6aa90415fa
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-10 17:44:56 +0530

    added test cases

--- src/test/java/com/neec/service/AuthenticationServiceImplTest.java
+++ src/test/java/com/neec/service/AuthenticationServiceImplTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -25,9 +27,12 @@ import org.springframework.test.util.ReflectionTestUtils;
 import com.neec.dto.LoginRequestDTO;
 import com.neec.dto.RegistrationRequestDTO;
 import com.neec.entity.UserLogin;
+import com.neec.enums.EnumRole;
 import com.neec.enums.EnumUserAccountStatus;
+import com.neec.exception.UserAccountSuspendedException;
 import com.neec.exception.UserAlreadyExistsException;
 import com.neec.exception.UserNotFoundException;
+import com.neec.exception.UserNotVerifiedException;
 import com.neec.repository.UserLoginRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +43,8 @@ public class AuthenticationServiceImplTest {
 	private BCryptPasswordEncoder mockBCryptPasswordEncoder;
 	@Mock
 	private RabbitTemplate mockRabbitTemplate;
+	@Mock
+	private JwtService mockJwtService;
 	@InjectMocks
 	private AuthenticationServiceImpl authenticationServiceImpl;
 	
@@ -68,6 +75,7 @@ public class AuthenticationServiceImplTest {
 		UserLogin mockSavedUser = UserLogin.builder()
 				.userLoginId(1L)
 				.emailAddress("new.email.address@gmail.com")
+				.verificationToken("7775c963-1d25-4f0b-b89d-b1b1d88cc4cb")
 				.accountStatus(EnumUserAccountStatus.ACTIVE)
 				.build();
 		when(mockUserLoginRepository.findByEmailAddress("new.email.address@gmail.com"))
@@ -139,6 +147,121 @@ public class AuthenticationServiceImplTest {
 			.thenReturn(Optional.empty());
 		UserNotFoundException ex = assertThrows(UserNotFoundException.class,
 				() -> authenticationServiceImpl.login(loginRequestDTO));
+		verify(mockUserLoginRepository).findByEmailAddress("unavailable.email.address@gmail.com");
 		assertEquals("invalid email or password.", ex.getMessage());
+	}
+
+	@Test
+	void testLogin_EmailAddressAvailable_InvalidPassword_RaiseUserNotFoundException() {
+		LoginRequestDTO loginRequestDTO = LoginRequestDTO.builder()
+				.emailAddress("available.email.address@gmail.com")
+				.password("invalid-password")
+				.build();
+		Optional<UserLogin> optUserLogin =
+				Optional.of(
+						UserLogin.builder()
+						.hashedPassword("hashed-password")
+						.build()
+				);
+		when(mockUserLoginRepository.findByEmailAddress(any(String.class)))
+			.thenReturn(optUserLogin);
+		when(mockBCryptPasswordEncoder.matches(any(String.class), any(String.class)))
+			.thenReturn(false);
+		UserNotFoundException ex =
+				assertThrows(UserNotFoundException.class,
+						() -> authenticationServiceImpl.login(loginRequestDTO));
+		verify(mockUserLoginRepository).findByEmailAddress(any(String.class));
+		verify(mockBCryptPasswordEncoder).matches(any(String.class), any(String.class));
+		assertEquals("invalid email or password.", ex.getMessage());
+	}
+
+	@Test
+	void testLogin_ValidLoginCredentials_AccountIsSuspended() {
+		LoginRequestDTO loginRequestDTO = LoginRequestDTO.builder()
+				.emailAddress("available.email.address@gmail.com")
+				.password("valid-password")
+				.build();
+		Optional<UserLogin> optUserLogin =
+				Optional.of(
+						UserLogin.builder()
+						.hashedPassword("hashed-password")
+						.accountStatus(EnumUserAccountStatus.SUSPENDED)
+						.build()
+				);
+		when(mockUserLoginRepository.findByEmailAddress(any(String.class)))
+			.thenReturn(optUserLogin);
+		when(mockBCryptPasswordEncoder.matches(any(String.class), any(String.class)))
+			.thenReturn(true);
+		UserAccountSuspendedException ex =
+				assertThrows(UserAccountSuspendedException.class,
+						() -> authenticationServiceImpl.login(loginRequestDTO));
+		verify(mockUserLoginRepository).findByEmailAddress(any(String.class));
+		verify(mockBCryptPasswordEncoder).matches(any(String.class), any(String.class));
+		assertEquals("Your account is suspended. Please contact Administrator",
+				ex.getMessage());
+	}
+
+	@Test
+	void testLogin_ValidLoginCredentials_AccountPendingVerification() {
+		LoginRequestDTO loginRequestDTO = LoginRequestDTO.builder()
+				.emailAddress("available.email.address@gmail.com")
+				.password("valid-password")
+				.build();
+		Optional<UserLogin> optUserLogin =
+				Optional.of(
+						UserLogin.builder()
+						.hashedPassword("hashed-password")
+						.accountStatus(EnumUserAccountStatus.PENDING_VERIFICATION)
+						.build()
+				);
+		when(mockUserLoginRepository.findByEmailAddress(any(String.class)))
+			.thenReturn(optUserLogin);
+		when(mockBCryptPasswordEncoder.matches(any(String.class), any(String.class)))
+			.thenReturn(true);
+		UserNotVerifiedException ex =
+				assertThrows(UserNotVerifiedException.class,
+						() -> authenticationServiceImpl.login(loginRequestDTO));
+		verify(mockUserLoginRepository).findByEmailAddress(any(String.class));
+		verify(mockBCryptPasswordEncoder).matches(any(String.class), any(String.class));
+		assertEquals("Your account is not verified. Please check your email.",
+				ex.getMessage());
+	}
+
+	@Test
+	void testLogin_TestJwtGeneration() {
+		LoginRequestDTO loginRequestDTO = LoginRequestDTO.builder()
+				.emailAddress("available.email.address@gmail.com")
+				.password("valid-password")
+				.build();
+		Optional<UserLogin> optUserLogin =
+				Optional.of(
+						UserLogin.builder()
+						.userLoginId(1L)
+						.emailAddress("available.email.address@gmail.com")
+						.hashedPassword("hashed-password")
+						.accountStatus(EnumUserAccountStatus.ACTIVE)
+						.role(EnumRole.APPLICANT)
+						.build()
+				);
+		when(mockUserLoginRepository.findByEmailAddress(any(String.class)))
+			.thenReturn(optUserLogin);
+		when(mockBCryptPasswordEncoder.matches(any(String.class), any(String.class)))
+			.thenReturn(true);
+		String mockJwtToken = "mock-jwt-token";
+		when(mockJwtService.generateJwtToken(anyLong(), anyString(), anyString()))
+			.thenReturn(mockJwtToken);
+		String jwtToken = authenticationServiceImpl.login(loginRequestDTO);
+		verify(mockUserLoginRepository).findByEmailAddress(any(String.class));
+		verify(mockBCryptPasswordEncoder).matches(any(String.class), any(String.class));
+
+		ArgumentCaptor<Long> userIdCaptor = ArgumentCaptor.forClass(Long.class);
+		ArgumentCaptor<String> emailAddressCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> userRoleCaptor = ArgumentCaptor.forClass(String.class);
+		verify(mockJwtService).generateJwtToken(userIdCaptor.capture(),
+				emailAddressCaptor.capture(), userRoleCaptor.capture());
+		assertEquals(1L, userIdCaptor.getValue());
+		assertEquals("available.email.address@gmail.com", emailAddressCaptor.getValue());
+		assertEquals(EnumRole.APPLICANT.name(), userRoleCaptor.getValue());
+		assertEquals("mock-jwt-token", jwtToken);
 	}
 }
