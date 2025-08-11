6cbfcbf362b2f3f58c5c143bf5d6d78ec6e07b52
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-11 21:50:44 +0530

    added further test cases

--- src/test/java/com/neec/controller/AuthenticationControllerTest.java
+++ src/test/java/com/neec/controller/AuthenticationControllerTest.java
@@ -4,13 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -29,6 +33,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neec.config.SecurityConfig;
 import com.neec.dto.LoginRequestDTO;
 import com.neec.dto.RegistrationRequestDTO;
+import com.neec.exception.InvalidTokenException;
 import com.neec.exception.UserAccountSuspendedException;
 import com.neec.exception.UserAlreadyExistsException;
 import com.neec.exception.UserNotFoundException;
@@ -237,6 +242,88 @@ public class AuthenticationControllerTest {
 		assertEquals(HttpStatus.UNAUTHORIZED.value(), result.getResponse().getStatus());
 		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
 		assertEquals("Your account is not verified. Please check your email.", jsonNode.get("error").asText());
+	}
+
+	@Test
+	void testLogin_ValidLoginCredentials_AccountVerified_Return_200_OK() throws Exception {
+		LoginRequestDTO dto = LoginRequestDTO.builder()
+				.emailAddress("email.address.found@gmail.com")
+				.password("valid-password")
+				.build();
+		when(mockAuthenticationService.login(any(LoginRequestDTO.class)))
+			.thenReturn("jwt-token");
+		RequestBuilder request = MockMvcRequestBuilders.post("/api/v1/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJsonString(dto));
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andReturn();
+		verify(mockAuthenticationService, times(1)).login(any(LoginRequestDTO.class));
+		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
+		assertEquals("jwt-token", jsonNode.get("jwtToken").asText());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"/api/v1/auth/verify", "/api/v1/auth/verify?", "/api/v1/auth/verify?t",
+			"/api/v1/auth/verify?tokn"})
+	void testVerifyEmailAddress_URL_Without_Token_Word(String url) throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get(url);
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andReturn();
+		assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
+		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
+		assertEquals("Request parameter 'token' is required.", jsonNode.get("error").asText());
+	}
+
+	@Test
+	void testVerifyEmailAddress_URL_With_EmptyToken() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/auth/verify?token=");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andReturn();
+		assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
+		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
+		assertEquals("Verification token can not be empty.", jsonNode.get("error").asText());
+	}
+
+	@Test
+	void testVerifyEmailAddress_URL_With_InvalidToken() throws Exception {
+		doThrow(new InvalidTokenException("invalid or expired verification token."))
+			.when(mockAuthenticationService).verifyUser(anyString());
+		RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/auth/verify?token=invalid-token");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andReturn();
+		assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
+		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
+		assertEquals("invalid or expired verification token.", jsonNode.get("error").asText());
+	}
+
+	@Test
+	void testVerifyEmailAddress_URL_With_ExpiredToken() throws Exception {
+		doThrow(new InvalidTokenException("Verification token has expired."))
+			.when(mockAuthenticationService).verifyUser(anyString());
+		RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/auth/verify?token=expired-token");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andReturn();
+		assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
+		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
+		assertEquals("Verification token has expired.", jsonNode.get("error").asText());
+	}
+
+	@Test
+	void testVerifyEmailAddress_URL_With_ActiveToken() throws Exception {
+		doNothing().when(mockAuthenticationService).verifyUser(anyString());
+		RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/auth/verify?token=active-token");
+		MvcResult result = mockMvc.perform(request)
+				.andDo(print())
+				.andReturn();
+		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+		JsonNode jsonNode = toJsonNode(result.getResponse().getContentAsString());
+		assertEquals("Your email address has been successfully verified. You can now log in.", jsonNode.get("status").asText());
 	}
 
 	private String toJsonString(RegistrationRequestDTO dto) throws JsonProcessingException {
