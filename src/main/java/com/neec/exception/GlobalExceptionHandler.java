242b11c47cd0269cb064319dbab2ad8d4c51a1e8
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-11 09:19:13 +0530

    added exception handler methods for verifying user email address verification token

--- src/main/java/com/neec/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neec/exception/GlobalExceptionHandler.java
@@ -8,8 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,11 +25,21 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.badRequest().body(errors);
 	}
 
+	@ExceptionHandler(exception = {MissingServletRequestParameterException.class})
+	public ResponseEntity<Map<String, String>> handleMissingServletRequestParameterException(MissingServletRequestParameterException ex){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "Request parameter 'token' is required."));
+	}
+
+	@ExceptionHandler(exception = {ConstraintViolationException.class})
+	public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "Verification token can not be empty."));
+	}
+
 	@ExceptionHandler(exception = {UserAlreadyExistsException.class})
 	public ResponseEntity<Map<String, String>> handleUserAlreadyExistsException(UserAlreadyExistsException ex){
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("registration_status", ex.getMessage()));
 	}
-	
+
 	@ExceptionHandler(exception = {HttpMessageNotReadableException.class})
 	public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex){
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Request_Body_Error", "Please check request body"));
@@ -35,6 +48,11 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(exception = {UserNotFoundException.class, UserAccountSuspendedException.class,
 			UserNotVerifiedException.class})
 	public ResponseEntity<Map<String, String>> handleAuthenticationExceptions(RuntimeException ex){
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", ex.getMessage()));
+	}
+
+	@ExceptionHandler(exception = {InvalidTokenException.class})
+	public ResponseEntity<Map<String, String>> handleInvalidTokenException(InvalidTokenException ex){
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", ex.getMessage()));
 	}
 }
